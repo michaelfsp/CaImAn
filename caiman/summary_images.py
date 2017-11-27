a9701a7c89983f934729a21e2815b964c9d286e2
Michael Pereira <michael.fsp@gmail.com>
2017-11-27 15:48:54 +0000

    make local_correlations() robust to movies containing pixels with zero variance (e.g. stuck / damaged pixels)

--- caiman/summary_images.py
+++ caiman/summary_images.py
@@ -156,7 +156,7 @@ def local_correlations(Y, eight_neighbours=True, swap_dim=True):
         Y = np.transpose(Y, tuple(np.hstack((Y.ndim - 1, list(range(Y.ndim))[:-1]))))
 
     rho = np.zeros(np.shape(Y)[1:])
-    w_mov = old_div((Y - np.mean(Y, axis=0)), np.std(Y, axis=0))
+    w_mov = old_div((Y - np.mean(Y, axis=0)), np.std(Y, axis=0).clip(min=1e-14))
 
     rho_h = np.mean(np.multiply(w_mov[:, :-1, :], w_mov[:, 1:, :]), axis=0)
     rho_w = np.mean(np.multiply(w_mov[:, :, :-1], w_mov[:, :, 1:]), axis=0)
